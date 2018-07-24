b175965c934a4ada68b032be4e250823b4eb19ba
jgc <guocui.jiang@socialcredits.cn>
2018-07-24 14:25:09 +0800

    zuul ratelimit

--- api-gateway/src/main/java/com/jgc/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jgc/apigateway/ApiGatewayApplication.java
@@ -3,10 +3,8 @@ package com.jgc.apigateway;
 import com.jgc.apigateway.filters.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
@@ -22,4 +20,6 @@ public class ApiGatewayApplication {
     public AccessFilter accessFilter() {
         return new AccessFilter();
     }
+
+
 }
